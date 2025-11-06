dc9713bbb5539c557f2b35502be4b6079cf64407
BRMolinero <barbaramolinero7@gmail.com>
2025-11-06 16:45:18 -0300

    Actualización de sw.js y LoadingMessage.jsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5stnm4ldkdo"
+    "revision": "0.1p9e6pis608"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
